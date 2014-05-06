ed2f413d0a5a884a9d39126b08cd6f7c0d426465
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-05-06 11:10:54 -0300

    Inserção do menu
    menu provisório

--- UCDiagram/src/Tela.java
+++ UCDiagram/src/Tela.java
@@ -1,29 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class Tela {
 
 	JFrame janela;
 	JMenuBar barraMenu;
-	JMenu menuArquivo;
-	JMenuItem itemNovo;
+	JMenu menuArquivo, menuAjuda;
+	JMenuItem itemNovo, itemAbrir, itemCarregar, itemSair;
+	JList<String> listaProjeto;
+	DefaultListModel<String> acoesProjeto;
+	JPanel painelProjeto;
 	
 	public Tela(){
 		instanciarComponentes();
+		janela.getContentPane().setLayout(new BorderLayout());
+		
+		iniciarBarraMenus();
+		iniciarListaProjetos();
+		
 		
 		setConfigFrame();
 	}
 	
 	private void instanciarComponentes(){
-		janela = new JFrame("UCDiagram - Sistema de Cadastros");
+		janela = new JFrame("UCDiagram - Sistema Casos de Uso");
 		barraMenu = new JMenuBar();
+		menuAjuda = new JMenu("Ajuda");
 		menuArquivo = new JMenu("Arquivo");
 		itemNovo = new JMenuItem("Novo");
+		itemAbrir = new JMenuItem("Abrir");
+		itemCarregar = new JMenuItem("Carregar");
+		itemSair = new JMenuItem("Sair");
+		acoesProjeto = new DefaultListModel<>();
+		listaProjeto = new JList<String>(acoesProjeto);
+		painelProjeto = new JPanel();
 	}
 	
-	private void setConfigFrame(){
+	private void iniciarBarraMenus(){
+		menuArquivo.add(itemNovo);
+		menuArquivo.add(itemAbrir);
+		menuArquivo.add(itemCarregar);
+		menuArquivo.addSeparator();
+		menuArquivo.add(itemSair);
+		barraMenu.add(menuArquivo);
+		barraMenu.add(Box.createHorizontalGlue());
+		barraMenu.add(menuAjuda);
+		janela.add(barraMenu, BorderLayout.NORTH);
+	}
+	
+	private void iniciarListaProjetos(){
+		janela.add(listaProjeto, BorderLayout.WEST);
+	}
+	
+	private void setConfigFrame(){		
 		janela.setDefaultCloseOperation(janela.EXIT_ON_CLOSE);
 		janela.setSize(800, 600);
 		janela.setVisible(true);
