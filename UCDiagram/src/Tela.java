e9e2f7bddc522b08e083d4bf6e97705e0329657c
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-05-13 16:15:08 -0300

    removido menu e inserido barra de ferramentas

--- UCDiagram/src/Tela.java
+++ UCDiagram/src/Tela.java
@@ -1,21 +1,18 @@
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-
-import javax.swing.Box;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
 public class Tela {
 
 	JFrame janela;
-	JMenuBar barraMenu;
-	JMenu menuArquivo, menuAjuda;
-	JMenuItem itemNovo, itemAbrir, itemCarregar, itemSair;
+	JToolBar barraFerramentas;
+	ImageIcon iconeAbrir, iconeAtor, iconeNovo, iconeSalvar, iconeUseCase;
+	JButton btnIconeAbrir, btnIconeAtor, btnIconeNovo, btnIconeSalvar, btnIconeUseCase;
 	JList<String> listaProjeto;
 	DefaultListModel<String> acoesProjeto;
 	JPanel painelProjeto;
@@ -24,7 +21,7 @@ public class Tela {
 		instanciarComponentes();
 		janela.getContentPane().setLayout(new BorderLayout());
 		
-		iniciarBarraMenus();
+		iniciarBarraFerramentas();
 		iniciarListaProjetos();
 		
 		
@@ -33,28 +30,36 @@ public class Tela {
 	
 	private void instanciarComponentes(){
 		janela = new JFrame("UCDiagram - Sistema Casos de Uso");
-		barraMenu = new JMenuBar();
-		menuAjuda = new JMenu("Ajuda");
-		menuArquivo = new JMenu("Arquivo");
-		itemNovo = new JMenuItem("Novo");
-		itemAbrir = new JMenuItem("Abrir");
-		itemCarregar = new JMenuItem("Carregar");
-		itemSair = new JMenuItem("Sair");
+		
+		barraFerramentas = new JToolBar();
+		
+		iconeAbrir = new ImageIcon("imagens/abrir.gif");
+		iconeAtor = new ImageIcon("imagens/ator.gif");
+		iconeNovo = new ImageIcon("imagens/novo.jpg");
+		iconeSalvar = new ImageIcon("imagens/salvar.png");
+		iconeUseCase = new ImageIcon("imagens/useCase.gif");
+		
+		btnIconeAbrir = new JButton(iconeAbrir);
+		btnIconeAtor = new JButton(iconeAtor);
+		btnIconeNovo = new JButton(iconeNovo);
+		btnIconeSalvar = new JButton(iconeSalvar);
+		btnIconeUseCase = new JButton(iconeUseCase);
+		
 		acoesProjeto = new DefaultListModel<>();
+		
 		listaProjeto = new JList<String>(acoesProjeto);
+		
 		painelProjeto = new JPanel();
 	}
 	
-	private void iniciarBarraMenus(){
-		menuArquivo.add(itemNovo);
-		menuArquivo.add(itemAbrir);
-		menuArquivo.add(itemCarregar);
-		menuArquivo.addSeparator();
-		menuArquivo.add(itemSair);
-		barraMenu.add(menuArquivo);
-		barraMenu.add(Box.createHorizontalGlue());
-		barraMenu.add(menuAjuda);
-		janela.add(barraMenu, BorderLayout.NORTH);
+	private void iniciarBarraFerramentas(){
+		barraFerramentas.add(btnIconeNovo);
+		barraFerramentas.add(btnIconeAbrir);
+		barraFerramentas.add(btnIconeSalvar);
+		barraFerramentas.addSeparator();
+		barraFerramentas.add(btnIconeAtor);
+		barraFerramentas.add(btnIconeUseCase);
+		janela.add(barraFerramentas, BorderLayout.NORTH);
 	}
 	
 	private void iniciarListaProjetos(){
