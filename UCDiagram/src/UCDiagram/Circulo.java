483556a2b95b182ec343bcc9900466ba51a64f99
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-05-25 23:43:25 -0300

    up

---
+++ UCDiagram/src/UCDiagram/Circulo.java
@@ -0,0 +1,21 @@
+package UCDiagram;
+
+
+import java.awt.Graphics;
+
+
+public class Circulo extends Figura {
+	int x, y;
+	int tam = 10;
+
+	public Circulo(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public void desenhar(Graphics desenho) {
+		desenho.fillOval(x, y, tam, tam);
+	}
+
+}
---
+++ UCDiagram/src/UCDiagram/Figura.java
@@ -0,0 +1,8 @@
+package UCDiagram;
+
+
+import java.awt.Graphics;
+
+abstract class Figura {
+	public abstract void desenhar(Graphics g);
+}
