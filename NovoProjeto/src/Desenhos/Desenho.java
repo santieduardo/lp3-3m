43a3e7adce26f9a9a1cd290b8daf5edc771f70e2
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-05-27 08:27:46 -0300

    removendo projeto

--- NovoProjeto/src/Desenhos/Desenho.java
+++
@@ -1,7 +0,0 @@
-package Desenhos;
-
-import java.awt.Graphics;
-
-public abstract class Desenho {
-	public abstract void desenha(Graphics g);
-}
--- NovoProjeto/src/projetoDesenho/ProjetoDesenho.java
+++
@@ -1,7 +0,0 @@
-package projetoDesenho;
-
-public class ProjetoDesenho {
-	public static void main(String[] args) {
-		new DesenhoFrame();
-	}
-}
